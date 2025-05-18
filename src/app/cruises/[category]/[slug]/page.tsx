d2194b122d03ecb34b1af1ef27c5e0e64cd2ee31
AaryanNReal <aaryansharmawork@gmail.com>
2025-05-18 05:40:02 +0530

    Initial commit

--- src/app/cruises/[category]/[slug]/page.tsx
+++ src/app/cruises/[category]/[slug]/page.tsx
@@ -33,12 +33,6 @@ export async function generateMetadata({
             alt: cruise.title,
           }],
         },
-        twitter: {
-          card: 'summary_large_image',
-          title: cruise.title,
-          description: cruise.description.substring(0, 160),
-          images: [cruise.imageURL],
-        },
       };
     }
   } catch (error) {
@@ -51,8 +45,12 @@ export async function generateMetadata({
   };
 }
 
-// This is the key fix - we don't try to use params in the Page component
-// because the client component will handle that itself
-export default function Page() {
+interface PageProps {
+  params: {
+    slug: string;
+  };
+}
+
+export default function Page({ params }: PageProps) {
   return <CruiseDetail />;
 }

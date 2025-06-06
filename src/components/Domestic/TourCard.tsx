eab7e028c4a33336ffc5aaee8927c5c96f7a7851
AaryanNReal <aaryansharmawork@gmail.com>
2025-06-07 02:17:04 +0530

    first commit

--- src/components/Domestic/TourCard.tsx
+++ src/components/Domestic/TourCard.tsx
@@ -115,7 +115,7 @@ const TourCard: React.FC<TourCardProps> = ({
           <div className="flex items-center justify-between text-sm text-gray-500 dark:text-gray-400 border-t border-black/10 pt-3">
             <div className="flex items-center">
               <MapPinIcon className="w-5 h-5 mr-1.5" />
-              <span className="">{location}</span>
+              <span className="line-clamp-1">{location}</span>
             </div>
             
             <div className="flex items-center">
